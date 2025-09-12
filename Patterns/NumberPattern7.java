ceb85f164eaf12fc9c3cbe8d8332150d975bd0d3
Parvatham Ram Charan <parvathamramcharan7@gmail.com>
2025-09-12 23:32:48 +0530

    Update NumberPattern7.java

--- Patterns/NumberPattern7.java
+++ Patterns/NumberPattern7.java
@@ -16,8 +16,31 @@
 public class NumberPattern7 {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        for(int i = n ; i >= 1 ;i--){
-             
+        int spaces = 0, count = 1;
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print("  ");
+            }
+            spaces++;
+            count = 1;
+            for (int k = 1; k <= i; k++) {
+                System.out.print(count + " ");
+                count++;
+            }
+            System.out.println();
+        }
+        spaces = n - 2;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print("  ");
+            }
+            spaces--;
+            count = 1;
+            for (int k = 1; k <= i; k++) {
+                System.out.print(count + " ");
+                count++;
+            }
+            System.out.println();
         }
     }
 }
