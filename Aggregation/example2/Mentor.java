af756408b4f7b6bc67bd71e4d3dcdf6770437d52
Parvatham Ram Charan <parvathamramcharan7@gmail.com>
2025-09-17 22:00:07 +0530

    Update Mentor.java

--- Aggregation/example2/Mentor.java
+++ Aggregation/example2/Mentor.java
@@ -1,15 +1,18 @@
-import java.util.*;
+import java.util.ArrayList;
 public class Mentor {
     private String name;
     private int id;
     private ArrayList<Student> student;
-    Mentor(String name,int id,ArrayList<Student> student){
-        this.name = name;
-        this.id= id;
-        this.student= student;
-    }
-    @Override
-    public String toString(){
-        return name+" "+id+" "+student;
-    }
+     Mentor(String name , int id){
+        this.name =name;
+        this.id = id;
+        this.student= new ArrayList<>();
+     }
+     public void addStudent(Student s){
+      student.add(s);
+     }
+     @Override
+     public String toString(){
+        return name+" "+id+" "+student;  
+     }
 }
