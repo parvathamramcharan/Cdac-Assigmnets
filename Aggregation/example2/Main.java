36a82c8ec0845bc4b2f03af9966d8dad4ace607e
Parvatham Ram Charan <parvathamramcharan7@gmail.com>
2025-09-17 21:59:45 +0530

    Update Main.java

--- Aggregation/example2/Main.java
+++ Aggregation/example2/Main.java
@@ -1,18 +1,19 @@
 import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Student s1 = new Student("ram",67,"dac");
+         Student s1 = new Student("ram",67,"dac");
         Student s2 = new Student("raju",45,"dbda");
         Student s3 = new Student("sita",90,"desd");
         Student s4 = new Student("bheem",78,"dac");
         Student s5 = new Student("mahesh",23,"desd");
-        ArrayList<Student> arr = new ArrayList<>();
-        arr.add(s1);
-        arr.add(s2);
-        arr.add(s3);
-        arr.add(s4);
-        arr.add(s5);
-        Mentor m = new Mentor("Harshith", 123, arr);
+        
+        Mentor m = new Mentor("Harshith", 123);
+        m.addStudent(s1);
+        m.addStudent(s2);
+        m.addStudent(s3);
+        m.addStudent(s4);
+        m.addStudent(s5);
         System.out.println(m);
     }
 }
