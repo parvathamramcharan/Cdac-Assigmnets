aa155bd05ff7143fd4980cdfe4a13fc560fe9b5a
Parvatham Ram Charan <parvathamramcharan7@gmail.com>
2025-09-17 19:45:43 +0530

    Update Main.java

--- access_modifiers/Main.java
+++ access_modifiers/Main.java
@@ -1,3 +1,7 @@
+//1.private -> data members  and also to nested classes
+//2. default -> (un specified) -> accessble with in our package; applied to everything
+//3.protected -> (default+ inheritence)=> parent class members accessed by class class members
+//4.public -> applied to class ,constructor,data...
 public class Main {
     public static void main(String[] args) {
         Person p = new Person("raju",10);
