cb1cb1272e394c910b2b95ef02ce3e8e8bb9a745
Parvatham Ram Charan <parvathamramcharan7@gmail.com>
2025-09-18 17:19:47 +0530

    Create ClassicCustomer.java]

---
+++ Eclipse(oops)3/inheritence/ClassicCustomer.java
@@ -0,0 +1,21 @@
+package inheritence;
+
+public class ClassicCustomer extends Customer {
+    private double irRate;
+	public ClassicCustomer(int cid, String cname, double balance,double irRate) {
+		super(cid, cname, balance);
+		this.irRate=irRate;
+	} 
+	@Override
+	public void dispCust() {
+		super.dispCust();
+		System.out.println("interest rate : "+irRate);
+	}
+	@Override
+	public void calInterest() {
+		double SI = (getBalance()*irRate*5)/100;
+		System.out.println("SI : "+SI);
+	}
+	
+      
+}
