97df0a52f2b209a3de4045c427afbdb5787305c5
Parvatham Ram Charan <parvathamramcharan7@gmail.com>
2025-09-18 17:09:18 +0530

    Delete override/example2 directory

--- override/example2/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Person p = new Person("ram", 20);
-        //System.out.println(p);  //Person@2f92e0f4  if not toString called or overridden
-        System.out.println(p.toString()); //ram 30
-        System.out.println(p);  //ram 30 
-    }
-}
