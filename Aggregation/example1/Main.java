e4e4eed918a2e2ace6c18444277ed8b7d032ff0c
Parvatham Ram Charan <parvathamramcharan7@gmail.com>
2025-09-18 16:54:37 +0530

    Delete Aggregation directory

--- Aggregation/example1/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Address addr =new Address(4113, "Temple road", "mustikuntla", 507204);
-        Person p = new Person("ram", 21, addr);
-        System.out.println(p);
-    }
-}
