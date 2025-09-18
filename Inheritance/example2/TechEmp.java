2467847cab2776c6b34ced491ab3cc07aeeae989
Parvatham Ram Charan <parvathamramcharan7@gmail.com>
2025-09-18 07:33:52 +0530

    Create TechEmp.java

---
+++ Inheritance/example2/TechEmp.java
@@ -0,0 +1,13 @@
+public class TechEmp  extends Emp{
+    TechEmp(int eid,String name,String city,double basic){
+    super(eid,name ,city,basic);
+    };
+}
+
+//here not setting  with method , if method then automatically constructor calls then super
+//but here constructor so we have to call do manual
+
+
+//parent(no constuctor) automatically
+//parent(constructor) manually
+
