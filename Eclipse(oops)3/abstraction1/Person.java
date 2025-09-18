a45d38194bc382f2f896079b70f451b4b4161448
Parvatham Ram Charan <parvathamramcharan7@gmail.com>
2025-09-18 17:18:52 +0530

    Create Person.java

---
+++ Eclipse(oops)3/abstraction1/Person.java
@@ -0,0 +1,8 @@
+package abstraction1;
+
+public abstract class Person {
+          public abstract void eat();
+          public void talk() {
+        	  System.out.println("speaks telugu");
+          }
+}
