5f9ea612525de8393837378458db805e9774dd80
Parvatham Ram Charan <parvathamramcharan7@gmail.com>
2025-09-19 22:58:55 +0530

    Update Driver3.java

--- Assignment8/problem9/Driver3.java
+++ Assignment8/problem9/Driver3.java
@@ -1,5 +1,9 @@
 package assigment8;
 
+//Problem 9: Anonymous Inner Class Extending an Abstract Class - Create an abstract
+//class Shape with an abstract method draw(). Write a method createShape() in another
+//class that uses an anonymous inner class to extend Shape and provides an
+//implementation for the draw() method
 public class Driver3 {
 	public void createShape() {
 	Shape s = new Shape() {
