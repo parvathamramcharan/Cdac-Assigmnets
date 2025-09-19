e9880f0ec3bca10fadbd6264b07daf8676e22973
Parvatham Ram Charan <parvathamramcharan7@gmail.com>
2025-09-19 23:21:23 +0530

    Create Person.java

---
+++ assignment7/problem3/Person.java
@@ -0,0 +1,11 @@
+package problem3;
+
+public class Person {
+   private String name;
+   public void setName(String name) {
+	   this.name = name;
+   }
+   public void display() {
+	   System.out.println(name);
+   }
+}
