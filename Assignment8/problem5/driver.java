549a9960601f16607d74b86e3ab8274b64e10ef0
Parvatham Ram Charan <parvathamramcharan7@gmail.com>
2025-09-19 22:42:07 +0530

    Create driver.java

---
+++ Assignment8/problem5/driver.java
@@ -0,0 +1,13 @@
+package problem5;
+//Problem 5: Create an interface Vehicle with a default method startEngine() that prints
+//"Engine started". Implement this interface in the class Car and override the
+//startEngine() method.
+public class Driver {
+
+	public static void main(String[] args) {
+		Vechile  v = new Car();
+		v.startEngine();
+
+	}
+
+}
