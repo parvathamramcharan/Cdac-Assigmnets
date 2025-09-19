90d97d77ba6c928bc8dca341b3592485b1047da5
Parvatham Ram Charan <parvathamramcharan7@gmail.com>
2025-09-19 22:54:41 +0530

    Update Driver1.java

--- Assignment8/problem7/Driver1.java
+++ Assignment8/problem7/Driver1.java
@@ -1,5 +1,9 @@
 package assigment8;
 
+//Problem 7: Interface with Multiple Implementations - Create an interface Payment with a
+//method pay(). Implement this interface in two classes CreditCardPayment and
+//PaypalPayment. Write a PaymentProcessor class that uses the Payment interface to
+//process payments
 public class Driver1 {
 
 	public static void main(String[] args) {
