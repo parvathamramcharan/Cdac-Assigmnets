0b15e6f1b5c559eee8eed9e6854f6d177d911e31
Parvatham Ram Charan <parvathamramcharan7@gmail.com>
2025-09-19 23:27:57 +0530

    Create Member.java

---
+++ assignment7/problem6/Member.java
@@ -0,0 +1,14 @@
+package problem6;
+
+public class Member {
+    String name;
+    int age;
+    String phoneNumber;
+    String address;
+    double salary;
+
+    // Method to print salary
+    void printSalary() {
+        System.out.println("Salary: " + salary);
+    }
+}
