372724f654bef264b9178193807ea2e2e236b1a8
Parvatham Ram Charan <parvathamramcharan7@gmail.com>
2025-09-19 22:59:28 +0530

    Update Driver4.java

--- Assignment8/problem10/Driver4.java
+++ Assignment8/problem10/Driver4.java
@@ -1,5 +1,7 @@
 package assigment8;
-
+//Problem 10: Anonymous Inner Class Extending a Regular Class - Create a class Printer
+//with a method printMessage(). Write a method createPrinter() in another class that uses
+//an anonymous inner class to extend Printer and overrides the printMessage() method.
 public class Driver4 {
     public void createPrinter() {
     	Printer p = new  Printer() {
