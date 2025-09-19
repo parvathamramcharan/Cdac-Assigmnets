88354f2e5777084a3ed277d3de70f1047504d61f
Parvatham Ram Charan <parvathamramcharan7@gmail.com>
2025-09-19 22:39:46 +0530

    Create Cat.java

---
+++ Assignment8/problem4/Cat.java
@@ -0,0 +1,17 @@
+package problem4;
+
+public class Cat implements Animal {
+
+	@Override
+	public void makeSound() {
+		System.out.println("cat meos");
+		
+	}
+
+	@Override
+	public void eat() {
+		System.out.println("cat eat fish");
+		
+	}
+
+}
