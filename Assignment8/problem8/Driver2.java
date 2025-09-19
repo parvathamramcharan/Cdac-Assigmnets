9818cf0f6ed01b721b30422d0d8507ec16a6230d
Parvatham Ram Charan <parvathamramcharan7@gmail.com>
2025-09-19 22:56:50 +0530

    Update Driver2.java

--- Assignment8/problem8/Driver2.java
+++ Assignment8/problem8/Driver2.java
@@ -1,5 +1,8 @@
 package assigment8;
-
+//Problem 8: Anonymous Inner Class Implementing an Interface - Create an interface
+//Greeting with a method sayHello(). Write a method generateGreeting() in another class
+//that uses an anonymous inner class to implement the Greeting interface and prints a
+//greeting message
 public class Driver2 {
     public void genarateGretting() {
     	Greeting g = new Greeting() {
